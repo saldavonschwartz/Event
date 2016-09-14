2a9cbd586cd55335dd50188c7adb4b06186021f4
Federico Saldarini <saldavonschwartz@gmail.com>
2016-09-14 02:15:49 -0700

    Initial Implementation

---
+++ Event.h
@@ -0,0 +1,117 @@
+
+//    The MIT License (MIT) 
+// 
+//    Copyright (c) 2016 Federico Saldarini 
+//    https://www.linkedin.com/in/federicosaldarini 
+//    https://github.com/saldavonschwartz 
+//    http://0xfede.io 
+// 
+// 
+//    Permission is hereby granted, free of charge, to any person obtaining a copy 
+//    of this software and associated documentation files (the "Software"), to deal 
+//    in the Software without restriction, including without limitation the rights 
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+//    copies of the Software, and to permit persons to whom the Software is 
+//    furnished to do so, subject to the following conditions: 
+// 
+//    The above copyright notice and this permission notice shall be included in all 
+//    copies or substantial portions of the Software. 
+// 
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+//    SOFTWARE. 
+
+#pragma once
+
+#include <functional>
+#include <unordered_map>
+#include <map>
+
+namespace OXFEDE {
+
+	template<class OwnerT, class KeyT, class SigT>
+	class Event;
+
+	template<class OwnerT, class KeyT, class RetT, class... ArgTs>
+	class Event<OwnerT, KeyT, RetT(ArgTs...)> {
+
+		friend OwnerT;
+		
+		template <class KeyT2, class EType>
+		friend struct std::pair;
+
+	public:
+		using Delegate = std::function<RetT(ArgTs...)>;
+
+		struct Binding {
+			KeyT key;
+			Delegate delegate;
+
+			Binding(KeyT key, Delegate delegate) :
+				key(key),
+				delegate(delegate) {
+			}
+
+			template <class T>
+			Binding(KeyT key, T* target, RetT(T::*fPtr)(ArgTs...)) :
+				key(key),
+				delegate([&target, fPtr](ArgTs... args) {
+				return (target->*fPtr)(args...); 
+			}) {
+			}
+		};
+
+		Event& operator +=(const Binding& binding) {
+			bindings[binding.key] = binding.delegate;
+			return *this;
+		}
+
+		Event& operator -=(KeyT key) {
+			bindings.erase(key);
+			return *this;
+		}
+
+		int size() const {
+			return bindings.size();
+		}
+
+	protected:
+		std::map<KeyT, Delegate> bindings;
+
+		Event() = default;
+		~Event() = default;
+
+		void operator()(ArgTs... args) {
+			auto temp = bindings;
+			for (auto binding : temp) {
+				binding.second(args...);
+			}
+		}
+
+		void clear() {
+			bindings.clear();
+		}
+	};
+
+	template<class OwnerT, class IdxT, class KeyT, class SigT>
+	class MultiEvent;
+
+	template<class OwnerT, class IdxT, class KeyT, class RetT, class... ArgTs>
+	class MultiEvent<OwnerT, IdxT, KeyT, RetT(ArgTs...)> {
+
+	public:
+		using EventT = Event<OwnerT, KeyT, RetT(ArgTs...)>;
+
+		EventT& operator[](IdxT idx) {
+			return events[idx];
+		}
+
+	protected:
+		std::unordered_map<IdxT, EventT> events;
+	};
+
+}
